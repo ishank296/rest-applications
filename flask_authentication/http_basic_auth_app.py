6d2142d434b2974f7b9e534a4ebb7ac16e6fa71a
Ishank <ishank296@gmail.com>
2022-11-29 22:31:00 +0530

    sample project to show flask authentication

---
+++ flask_authentication/http_basic_auth_app.py
@@ -0,0 +1,28 @@
+from flask_authentication import app,api
+from flask_restful import Resource
+from flask_httpauth import HTTPBasicAuth
+
+auth = HTTPBasicAuth()
+user_data = {
+    'john':'qwzx@#',
+    'alice':'pomn$%'
+}
+
+@auth.verify_password
+def verfiy_login(username,password):
+    if username in user_data:
+        return user_data[username]
+    return None
+
+
+class PrivateResourse(Resource):
+
+    @auth.login_required()
+    def get(self):
+        return {"message":"Successful Get request to Private Resource"}, 200
+
+
+api.add_resource(PrivateResourse,'/api/private')
+
+if __name__ == "__main__":
+    app.run(debug=True)
